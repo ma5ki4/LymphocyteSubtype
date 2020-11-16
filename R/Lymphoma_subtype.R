8c3a59c594987f358cc130da45702f8eaa81ae4d
ma5ki4 <ma510ki4da@gmail.com>
2020-11-16 00:54:32 -0800

    Initial commit

---
+++ R/Lymphoma_subtype.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+
+Lymphoma_subtype <- tribble(
+  ~Disease, "Nodular sclerosis cHL", "Lymphocyte-rich cHL", "Mixed cellularity cHL", "Lymphocyte-depleted cHL",
+  "Nodular lymphocyte predominant HL" "Diffuse large B-cell lymphoma", "Follicular lymphoma","Mucosa-associated lymphatic tissue lymphoma",
+  "Chronic lymphocytic lymphoma", "Mantle cell lymphoma", "Mediastinal large B-cell lymphoma", "Lymphoblastic lymphoma",
+  "Nodal marginal zone B-cell lymphoma", "Splenic marginal zone B-cell lymphoma", "Extranodal marginal zone B-cell lymphoma",
+  "Primary effusion lymphoma", "Burkitt lymphoma", "Lymphomatoid granulomatosis", "Peripheral T-cell lymphoma",
+  "Cutaneous T-cell lymphoma", "Anaplastic large cell lymphoma", "NK-cell lymphoma", "Hepatosplenic lymphoma")
