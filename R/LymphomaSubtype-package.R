a0f950992720c8e4497392211527842ce318c0ef
ma5ki4 <ma510ki4da@gmail.com>
2020-11-18 00:18:09 -0800

    Added a new file

---
+++ R/LymphomaSubtype-package.R
@@ -0,0 +1,8 @@
+#' Lymphoma Subtype Classification Data
+#'
+#' Use this package to load all the lymphoma subtypes as a tibble.
+#'
+#' @name LymphomaSubtype
+#' @author Makoto Kishida
+#' @docType package
+NULL
